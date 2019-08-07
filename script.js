5cedfa5c4130314b37756a4c9a0f350b34c30827
ariley42 <52224737+ariley42@users.noreply.github.com>
2019-08-06 19:25:16 -0700

    Add files via upload
    working list, now to increment the counter, add a check and delete functionality

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const app = new express();
-const port = 3000
-app.get('/', function(req, res) {
-  res.sendFile('/index.html', { root: __dirname });
-});
-app.listen(port, () =>
-console.log(`listening on port ${port}!`))
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
-}
+// const classNames = {
+//   TODO_ITEM: 'todo-container',
+//   TODO_CHECKBOX: 'todo-checkbox',
+//   TODO_TEXT: 'todo-text',
+//   TODO_DELETE: 'todo-delete',
+// }
 
-//const list = document.getElementById('todo-list')
-//const itemCountSpan = document.getElementById('item-count')
-//const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-function newTodo() {
-  alert('New TODO button clicked!')
+// const itemCountSpan = document.getElementById('item-count')
+// const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+//
+function newTodo(evt) {
+
+  evt.preventDefault();
+
+  const input = document.getElementById("todo-text").value;
+  const list = document.getElementById("todo-list");
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode("- " + input));
+  list.appendChild(li);
+  document.getElementById("todo-text").value = "";
+
 }
+    
+const form = document.getElementById("btn").addEventListener('click', newTodo);

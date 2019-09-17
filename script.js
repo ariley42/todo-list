a80c39a8afbe109d3c7b4a3450bf4ede673fb11d
ariley42 <52224737+ariley42@users.noreply.github.com>
2019-09-17 07:22:06 -0700

    Add files via upload
    todos display on screen!

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-// const classNames = {
-//   TODO_ITEM: 'todo-container',
-//   TODO_CHECKBOX: 'todo-checkbox',
-//   TODO_TEXT: 'todo-text',
-//   TODO_DELETE: 'todo-delete',
-// }
 
+const list = document.getElementById("todo-list");
 
-// const itemCountSpan = document.getElementById('item-count')
-// const uncheckedCountSpan = document.getElementById('unchecked-count')
-
-//
 function newTodo(evt) {
 
   evt.preventDefault();
-
   const input = document.getElementById("todo-text").value;
-  const list = document.getElementById("todo-list");
   const li = document.createElement("li");
   li.appendChild(document.createTextNode("- " + input));
   list.appendChild(li);
   document.getElementById("todo-text").value = "";
+  var checkbox = document.createElement('input');
+  checkbox.type= "checkbox";
+  checkbox.value = 1;
+  checkbox.class = "todo";
+  li.appendChild(checkbox);
+  checkbox.onchange = checkCount;
 
 }
     
 const form = document.getElementById("btn").addEventListener('click', newTodo);
+
+check = document.getElementsByClassName('todo');
+for (var i = 0; i < check.length; i++) {
+    check[i].addEventListener('click', newTodo, false);
+}
+
+
+
+const checkCount = function () {
+    var selected = [];
+    
+    var span = document.getElementById('item-count');
+    var span2 = document.getElementById('unchecked-count');
+    
+    var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+    
+    
+    for (var i = 0; i < checkboxes.length; i++) {
+           selected.push(checkboxes[i]);
+    
+    
+        }
+        span.innerHTML = selected.length;
+        
+    
+};
